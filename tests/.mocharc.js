09dfc8fa20e2b8e5df25caea21bb14133657ef81
Pal,Sayan <sayan.pal@netatwork.de>
2021-06-23 16:56:05 +0200

    chore: added moch-utils infra

--- tests/.mocharc.js
+++ tests/.mocharc.js
@@ -4,16 +4,9 @@ const isDev = !!process.env.DEV;
 module.exports = {
 	spec: './tests/**/*.spec.ts',
 	require: [path.resolve('./tests/ts-hook.js'), 'source-map-support/register'],
+	reporter: '@netatwork/mocha-utils/dist/JunitSpecReporter.js',
 	reporterOptions: {
 		mochaFile: './tests/.artifacts/results.xml'
 	},
-	...(isDev
-		? {
-			watch: true,
-			'watch-files': './**/*.ts',
-			reporter: 'spec'
-		}
-		: {
-			reporter: 'mocha-junit-reporter'
-		})
+	...(isDev ? { watch: true, 'watch-files': './**/*.ts' } : {})
 };
