338ab15b631e594cb02e6f21b0f190a1e6ba0363
Pal, Sayan <Sayan.Pal@netatwork.de>
2022-12-27 13:20:54 +0100

    chore: deps update

--- tests/.mocharc.js
+++ tests/.mocharc.js
@@ -5,8 +5,6 @@ module.exports = {
 	spec: './tests/**/*.spec.ts',
 	require: [path.resolve('./tests/ts-hook.js'), 'source-map-support/register'],
 	reporter: '@netatwork/mocha-utils/dist/JunitSpecReporter.js',
-	reporterOptions: {
-		mochaFile: './tests/.artifacts/results.xml'
-	},
+	reporterOptions:  ['mochaFile=./tests/.artifacts/results.xml'],
 	...(isDev ? { watch: true, 'watch-files': './**/*.ts' } : {})
 };
